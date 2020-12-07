053f319100a932a94959054d8f998b0690546b1b
root <root@Jonah.localdomain>
2020-12-07 15:50:21 +0100

    uppdaterad

--- 1_intro.js
+++ 1_intro.js
@@ -8,3 +8,11 @@ const x = things[3]
 
 // Längden av en array
 console.log(`Arrayen har ${things.length} element i sig`)
+
+const saker = ["bil", "hund", "träd", "tak"]
+
+saker[1] = "cat"
+
+const y = saker[3]
+
+console.log(`Det finns såhär ${things.length} många saker i lådan`)
